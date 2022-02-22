b4a33892ad4c603bd8385e8414c6a0b9e92928b3
Madhu Manepalli <87767379+MadhuManepalli@users.noreply.github.com>
2022-02-22 14:16:41 +0530

    Create Q1.java

---
+++ corejava/string assignments/Q1.java
@@ -0,0 +1,11 @@
+public class Q1 {
+		 
+	    public static void main(String[] args) 
+	    {
+	        String str = "hello world";
+	        int c = str.length();
+	        System.out.println("The String has " +c +" characters");
+	 
+	    }
+	 
+	}
