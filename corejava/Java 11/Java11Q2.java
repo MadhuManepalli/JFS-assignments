4c71429e86cb8a97730b4f48d1180e3e6c95402b
Madhu Manepalli <87767379+MadhuManepalli@users.noreply.github.com>
2022-03-02 21:46:43 +0530

    Add files via upload

---
+++ corejava/Java 11/Java11Q2.java
@@ -0,0 +1,25 @@
+
+class Java11Q2 {
+  
+    public static void main(String[] args)
+    {
+  
+        //the scenarios where var keyword cannot be used for such variable declarations//
+    	
+    	//var x = 50;//
+    	//var<var> al = new ArrayList<>();//
+    	//var<var> al = new ArrayList<>();//
+    	
+        var a = 421;    
+        var b = 9.4;
+        var c = 'M';
+        var d = "MADHU";
+        var e = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
