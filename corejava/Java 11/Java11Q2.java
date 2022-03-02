d0edc232d761aa645cdd6f89b8ed9be4cb6d5fff
Madhu Manepalli <87767379+MadhuManepalli@users.noreply.github.com>
2022-03-02 21:40:27 +0530

    Add files via upload

---
+++ corejava/Java 11/Java11Q2.java
@@ -0,0 +1,20 @@
+
+class Java11Q2 {
+  
+    public static void main(String[] args)
+    {
+  
+        
+        var a = 421;    
+        var b = 9.4;
+        var c = 'M';
+        var d = "MADHU";
+        var e = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
