67a135aa782316b36788bda2bf699f957fa62cae
Madhu Manepalli <87767379+MadhuManepalli@users.noreply.github.com>
2022-03-04 13:49:30 +0530

    Add files via upload

---
+++ JUnit/Lifecycle.java
@@ -0,0 +1,11 @@
+package io.junitbasics;
+
+public class Lifecycle {
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static int sub(int a, int b){
+        return a-b;
+    }
+
+}
