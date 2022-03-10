103ffee4a449f0862b6c635b2d1acf6c991098e1
Madhu Manepalli <87767379+MadhuManepalli@users.noreply.github.com>
2022-03-10 12:00:57 +0530

    Add files via upload

---
+++ springcore/src/main/java/springQ2/SpringQ2.java
@@ -0,0 +1,22 @@
+package springQ2;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringQ2{
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("springQ2.xml");	      
+	    Question qList= (Question) context.getBean("q2List");   
+	    System.out.println("\nList Injection\n"+qList);
+	    qList.Displayanswer(); 
+	   
+	    Question qset= (Question) context.getBean("q2Set");  
+	    System.out.println("\nSet Injection\n"+qset);
+	    qset.Displayanswer(); 
+	    
+	    Question qMap= (Question) context.getBean("q2Map");  
+	    System.out.println("\nMapInjection\n"+qMap.toStringMap());
+	    qMap.DisplayanswerM(); 
+	    context.close();
+	}
+}
