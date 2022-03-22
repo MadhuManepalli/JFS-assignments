5cf9d60cfe4a70b3c9c6cdfff5f226d64d02eda3
Madhu Manepalli <87767379+MadhuManepalli@users.noreply.github.com>
2022-03-22 09:56:21 +0530

    added springREST assignments

---
+++ SpringREST/SpringRestQ1/src/main/java/io/madhu/topic/TopicRepository.java
@@ -0,0 +1,10 @@
+package io.madhu.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+//logic for every entity class common one like update and get.
+public interface TopicRepository extends CrudRepository<Topic, String> {
+	
+	
+	
+}
---
+++ SpringREST/SpringRestQ1/src/test/java/io/madhu/SpringRestQ1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.madhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestQ1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
