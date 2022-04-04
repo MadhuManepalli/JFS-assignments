194fba99bfcd88c1b0dde76064ac0c4dbf87eadd
Madhu Manepalli <87767379+MadhuManepalli@users.noreply.github.com>
2022-04-04 11:54:15 +0530

    added assignments

--- javascript/functional  programming/BasicQ4.js
+++
@@ -1,6 +0,0 @@
-var array1 = [1, 2, 3, 4, 5];
-var map1 = array1.map(x => x * x);
-    document.write("Square: ",map1,"<br>");
-var array2 = [1, 4, 9, 16, 25];
-var map2 = array2.map(x => Math.sqrt(x));
-    document.write("Square root:",map2,"<br>");
