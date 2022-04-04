43332d44e56b80ff1780ba792ebbedcb263cdb57
Madhu Manepalli <87767379+MadhuManepalli@users.noreply.github.com>
2022-04-04 11:29:06 +0530

    added Javascript Assignments

---
+++ javascript/test.js
@@ -0,0 +1,35 @@
+var myNum = 10;
+if (myNum == 10){
+    document.write("myNum is equal to 10");
+}
+else{
+    document.write("myNum is not equal to 10");
+}
+
+
+var myAge = 21;
+
+if(myAge > 25){
+    document.write("you are over 25");
+}
+else if (myAge > 15){
+    document.write("you are over 15");
+}
+else if (myAge > 10){
+    document.write("you are over 10");
+}
+else{
+    document.write("you are not over 10");
+}
+
+var myAge = 21;
+if(myAge >= 18 && myAge <= 30){
+    document.write("you are eligible");
+}
+else{
+    document.write("you are not eligible");
+}
+
+
+
+
