55fff5d763460067998b3e73387271dc9fbe3cef
Madhu Manepalli <87767379+MadhuManepalli@users.noreply.github.com>
2022-04-05 14:23:03 +0530

    added ES6 and typescript assignments

---
+++ ES6 and Typescript/demo.ts
@@ -0,0 +1,5 @@
+console.log('Hello World' + ' from TypeScript'+ '!');
+console.log(`ES6 and typescript Assignments`);
+
+
+
---
+++ ES6 and Typescript/js/demo.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('Hello World' + ' from TypeScript' + '!');
+console.log(`ES6 and typescript Assignments`);
